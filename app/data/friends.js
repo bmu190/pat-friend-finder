216910f630b0bb18abfaf05be4f33814a1743cb6
pheel-up <plw1980@gmail.com>
2018-08-03 15:11:20 -0400

    (1) Added /html /body tags to survey.html (2) Moved routes to the root of the app folder

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends =   {
+        "name":"Ahmed",
+        "gender": "Male",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      };
+
+      module.exports = friends;
+
+      
+      
